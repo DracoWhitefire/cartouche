7de2e78c188730594eb9192760204675c6772373
Draco Whitefire <dracowhitefire@gmail.com>
2026-04-06 17:04:27 +0200

    Improve coverage

--- src/decoded.rs
+++ src/decoded.rs
@@ -150,3 +150,24 @@ impl<T: core::fmt::Debug, W: core::fmt::Debug> core::fmt::Debug for Decoded<T, W
             .finish()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn debug_impl_includes_value_and_warnings() {
+        let mut d: Decoded<u32, &str> = Decoded::new(42u32);
+        d.push_warning("something odd");
+        let s = alloc::format!("{d:?}");
+        assert!(s.contains("42"));
+        assert!(s.contains("something odd"));
+    }
+
+    #[test]
+    fn debug_impl_empty_warnings() {
+        let d: Decoded<u32, &str> = Decoded::new(7u32);
+        let s = alloc::format!("{d:?}");
+        assert!(s.contains("7"));
+    }
+}
