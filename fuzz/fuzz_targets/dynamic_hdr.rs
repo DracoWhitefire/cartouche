9e88940fd6f3c17d3377d5dd145568b8ceb786fb
Draco Whitefire <dracowhitefire@gmail.com>
2026-04-08 17:36:58 +0200

    fuzz target exercises SlHdrMetadata::decode()

--- fuzz/fuzz_targets/dynamic_hdr.rs
+++ fuzz/fuzz_targets/dynamic_hdr.rs
@@ -1,6 +1,6 @@
 #![no_main]
 
-use cartouche::dynamic_hdr::{DynamicHdrInfoFrame, Hdr10PlusMetadata};
+use cartouche::dynamic_hdr::{DynamicHdrInfoFrame, Hdr10PlusMetadata, SlHdrMetadata};
 use cartouche::encode::IntoPackets;
 use libfuzzer_sys::fuzz_target;
 
@@ -11,6 +11,8 @@ fuzz_target!(|data: &[u8]| {
 
     // Exercise Hdr10PlusMetadata::decode directly on arbitrary bytes.
     let _ = Hdr10PlusMetadata::decode(data, &mut |_| {});
+    // Exercise SlHdrMetadata::decode directly on arbitrary bytes.
+    let _ = SlHdrMetadata::decode(data, &mut |_| {});
 
     // Treat the fuzz input as a sequence of 31-byte packets.
     let packets: Vec<[u8; 31]> = data
